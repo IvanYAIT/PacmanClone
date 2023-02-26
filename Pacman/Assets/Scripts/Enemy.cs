6fdb38a39d5cef609dbb32506537e477b34dd1f0
IvanYAIT <83347588+IvanYAIT@users.noreply.github.com>
2023-02-26 19:44:27 +0300

    enemy random movement half

--- Pacman/Assets/Scripts/Enemy.cs
+++ Pacman/Assets/Scripts/Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float radius;
+    [SerializeField] private float speed;
 
     private Transform _player;
     private NavMeshAgent _agent;
@@ -17,42 +18,18 @@ public class Enemy : MonoBehaviour
         _player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        if (Vector2.Distance(transform.position, _player.position) > radius)
-            transform.Translate(new Vector3(0, 1, 0) * Time.fixedDeltaTime);
-        else
-            _agent.SetDestination(_player.position);
+        transform.Translate(new Vector3(0, 1, 0) * speed * Time.deltaTime);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void FixedUpdate()
     {
-        if (collision.CompareTag("Turn"))
-        {
-            Quaternion rotation = transform.rotation;
-            if (collision.GetComponent<Turn>().GetRegularTurn())
-            {
-                rotation.z += 90;
-            }
-            else
-            {
-                if (!collision.GetComponent<Turn>().GetThreeSides())
-                {
-                    _rnd = Random.Range(1, 2);
-                    if (_rnd == 1)
-                        rotation.z += 90;
-                }
-                else
-                {
-                    _rnd = Random.Range(1, 3);
-                    if (_rnd == 1)
-                        rotation.z += 90;
-                    else if (_rnd == 2)
-                        rotation.z -= 90;
-                }
-            }
+        
+        //if (Vector2.Distance(transform.position, _player.position) > radius)
             
-            transform.rotation = rotation;
-        }
+        //else
+        //    _agent.SetDestination(_player.position);
     }
 }
--- Pacman/Assets/Scripts/Turn.cs
+++ Pacman/Assets/Scripts/Turn.cs
@@ -2,9 +2,61 @@ using UnityEngine;
 
 public class Turn : MonoBehaviour
 {
-    [SerializeField] private bool regularTurn;
-    [SerializeField] private bool threeSides;
+    [SerializeField] private bool up;
+    [SerializeField] private bool down;
+    [SerializeField] private bool left;
+    [SerializeField] private bool right;
 
-    public bool GetThreeSides() => threeSides;
-    public bool GetRegularTurn() => regularTurn;
+    private bool _isTurned;
+
+    public void TurnEnemy(Transform enemy)
+    {
+        int rnd = Random.Range(1,4);
+        
+        if (up && rnd==1 && enemy.eulerAngles.z != 180)
+        {
+            enemy.rotation = new Quaternion();
+            enemy.Rotate(new Vector3(0, 0, 1), 0f);
+            Debug.Log("up");
+            _isTurned = true;
+        }
+        else if (right && rnd == 2 && enemy.eulerAngles.z != 90)
+        {
+            enemy.rotation = new Quaternion();
+            enemy.Rotate(new Vector3(0, 0, 1), -90);
+            Debug.Log("right");
+            _isTurned = true;
+        }
+        else if (left && rnd == 3 && enemy.eulerAngles.z != 270)
+        {
+            enemy.rotation = new Quaternion();
+            enemy.Rotate(new Vector3(0, 0, 1), 90);
+            Debug.Log("left");
+            _isTurned = true;
+        }
+        else if (down && rnd == 4 && enemy.eulerAngles.z != 0)
+        {
+            enemy.rotation = new Quaternion();
+            enemy.Rotate(new Vector3(0, 0, 1), 180);
+            Debug.Log("down");
+            _isTurned = true;
+        }
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy") && !_isTurned)
+        {
+            collision.transform.position = transform.position;
+            TurnEnemy(collision.transform);
+        }
+            
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+            _isTurned = false;
+    }
 }
